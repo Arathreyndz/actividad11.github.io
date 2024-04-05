5c6c6bc0cc71b69eace906a0f2d8f8c3f73ab749
Arathreyndz <164301354+Arathreyndz@users.noreply.github.com>
2024-04-04 23:05:30 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function growFlower() {
+    var flower = document.getElementById('flower');
+    var sizeIndicator = document.getElementById('sizeIndicator');
+    var currentSize = flower.offsetWidth;
+
+    // Incrementar el tamaño de la flor
+    var newSize = currentSize + 20;
+    flower.style.width = newSize + 'px';
+    flower.style.height = newSize + 'px';
+
+    // Actualizar el indicador de tamaño
+    sizeIndicator.innerHTML = 'Tamaño de la flor: ' + newSize + 'px';
+
+    // Agregar una línea para imprimir el valor de newSize en la consola del navegador
+    console.log("Tamano de la flor:", newSize);
+
+    // Cambiar la imagen cuando alcance cierto tamaño
+    if (newSize >= 300) {
+        flower.style.backgroundImage = "url('../images/flower.jpg')";
+    }
+}
